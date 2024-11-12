61cc49f5e2a13942e30005b7f33c785b04e06433
Aaryan6 <aaryanpatel683@gmail.com>
2024-11-12 16:06:01 +0530

    delete middleware

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  if (res.url.includes("chatwithpdf-y6adj.ondigitalocean.app")) {
-    return NextResponse.redirect("https://geminibot-5trwv.ondigitalocean.app/");
-  }
-  return res;
-}
